3be354dbd6ba4de38e2c758b8c276282df6e0211
Luckyjain17 <luckyjain0217@gmail.com>
2026-05-20 00:23:43 +0530

    allow_origins

--- app/main.py
+++ app/main.py
@@ -10,12 +10,13 @@ load_dotenv()
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[""],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+CORSMiddleware,
+allow_origins=["*"],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
 )
+
 
 app.include_router(call_router)
 
